4749c3313093c1f136477939927f24ab9720c7c4
MrWoafer <woafer1000@gmail.com>
2025-10-11 12:06:15 +0100

    Improve TypeEq definition

--- propositional/src/type_utils.rs
+++ propositional/src/type_utils.rs
@@ -1,5 +1,13 @@
-pub trait TypeEq<T> {}
-impl<T> TypeEq<T> for T {}
+/// This trait just says a type is equal to itself, for making [`assert_type_eq`] work.
+///
+/// This is automatically implemented on every type.
+pub trait TypeEq {
+    type SelfType;
+}
+
+impl<T> TypeEq for T {
+    type SelfType = Self;
+}
 
 /// Asserts that two types are the same, throwing a type-checking error if they are not.
 ///
@@ -16,6 +24,6 @@ impl<T> TypeEq<T> for T {}
 /// ```
 pub fn assert_type_eq<T, U>()
 where
-    T: TypeEq<U>,
+    T: TypeEq<SelfType = U>,
 {
 }
