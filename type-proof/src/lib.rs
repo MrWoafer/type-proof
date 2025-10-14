d3c3a780ae644d030096f9c1b6812c239cc1d669
MrWoafer <woafer1000@gmail.com>
2025-10-14 22:43:56 +0100

    Add rustdoc lints

--- type-proof/src/lib.rs
+++ type-proof/src/lib.rs
@@ -1,3 +1,15 @@
+#![deny(
+    rustdoc::broken_intra_doc_links,
+    rustdoc::private_intra_doc_links,
+    rustdoc::missing_crate_level_docs,
+    rustdoc::invalid_codeblock_attributes,
+    rustdoc::invalid_html_tags,
+    rustdoc::invalid_rust_codeblocks,
+    rustdoc::bare_urls,
+    rustdoc::unescaped_backticks
+)]
+#![warn(missing_docs)]
+
 //! A crate for type-checked propositional logic proofs.
 
 extern crate self as type_proof; // so that the path to this crate works in macros when used in this crate
